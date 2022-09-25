628f948f0fdca5192ac2b606b41f5240bfb4190a
saimarnpha42 <saimarnpha42@gmail.com>
2022-09-25 22:03:45 +0630

    Second commit

--- components/testimonial.js
+++ components/testimonial.js
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from 'react'
+import React,{ useState} from 'react'
 import Script from 'next/script'
 import Image from 'next/image';
 function Testimonial() {
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
 import 'styles/css/style.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css';
+import '@fortawesome/fontawesome-free/css/regular.min.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'styles/google-font.css'
 
